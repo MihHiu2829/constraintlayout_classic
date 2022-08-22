54815d7a2e375ddee0c54471968e86cc71bfb5ca
Minh_Hieu <himayato.2019.vn@gmail.com>
2022-08-22 15:43:47 +0700

    bao gồm nhiều thuộc tíh của constrainLayout + Cricle,CricleRadius,CỉcleAngle + các id +cóntrainVeritcal_bías

---
+++ app/src/main/java/com/example/project_40/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.project_40;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_m002_ver1);
+    }
+}
